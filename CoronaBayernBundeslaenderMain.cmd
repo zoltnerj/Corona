3e226601cc9c5df7d01936317269a21dcfaea673
zoltnerj <77570150+zoltnerj@users.noreply.github.com>
2021-01-17 17:11:09 +0100

    Add files via upload

---
+++ CoronaBayernBundeslaenderMain.cmd
@@ -0,0 +1,13 @@
+rem if "%JAVA_HOME%" == "" then Goto LABEL_UseProvidedJDK
+rem SET JAVA_HOME=M:\java\jdk1.8.0_111\bin
+rem goto LABEL_JDKWasAlreadyset
+:rem LABEL_UseProvidedJDK
+SET JAVA_HOME=.\jdk1.8.0_111\bin
+
+:LABEL_JDKWasAlreadyset
+
+SET JAVA
+set PATH=%JAVA_HOME%;%PATH%
+
+java -cp .\CoronaAnalyzer.jar jzo.corona.CoronaBayernBundeslaenderMain
+pause
---
+++ CoronaRomaniaMain.cmd
@@ -0,0 +1,14 @@
+rem if "%JAVA_HOME%" == "" then Goto LABEL_UseProvidedJDK
+rem SET JAVA_HOME=M:\java\jdk1.8.0_111\bin
+rem goto LABEL_JDKWasAlreadyset
+:rem LABEL_UseProvidedJDK
+SET JAVA_HOME=.\jdk1.8.0_111\bin
+
+:LABEL_JDKWasAlreadyset
+
+SET JAVA
+rem pause
+set PATH=%JAVA_HOME%;%PATH%
+
+java -cp .\CoronaAnalyzer.jar jzo.corona.CoronaRomaniaMain
+pause
